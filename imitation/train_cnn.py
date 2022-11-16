4fe6b716e53495bccdf0bc3099aa56b50b9c6953
Alonso94 <ay20-5-1994@hotmail.com>
2022-11-16 11:13:00 +0100

    imitation

--- imitation/train_cnn.py
+++ imitation/train_cnn.py
@@ -39,50 +39,59 @@ epochs=[2000, 5000, 10000]
 eval_type=['Demo', 'TestJitter']
 
 def run_experiment(model_name, params):
-    print('Running experiment {0}'.format(model_name))
+    print("Running experiment {0}".format(model_name))
     for key in params:
-        config[key]=params[key]
-    for i,env in enumerate(envs):
-        results={}
+        config[key] = params[key]
+    for i, env in enumerate(envs):
+        results = {}
         for eval in eval_type:
-            results[eval]=[]
-        config['epochs']=epochs[i]
+            results[eval] = []
+        config["epochs"] = epochs[i]
         for seed in seeds:
             set_seed(seed)
-            model_name_seed=model_name+"_{0}_seed={1}".format(env,seed)
-            config['model_name']=model_name_seed
-            wandb.init(project="Imitation", name=model_name_seed,
-                    # anonymous="allow",
-                    config=config, 
-                    # mode="disabled"
-                    )
-            agent=ImitationAgentCNN(config, env_name=env)
+            model_name_seed = model_name + "_{0}_seed={1}".format(env, seed)
+            config["model_name"] = model_name_seed
+            wandb.init(
+                project="Imitation",
+                name=model_name_seed,
+                group=model_name,
+                # anonymous="allow",
+                config=config,
+                # mode="disabled"
+            )
+            agent = ImitationAgentCNN(config, env_name=env)
             agent.train()
             for eval in eval_type:
                 agent.set_env(eval)
                 agent.evaluate_policy()
-                result=agent.report_results(eval)
+                result = agent.report_results(eval)
                 results[eval].append(result)
             wandb.run.finish()
-        wandb.init(project="Imitation", name=model_name+"_{0}".format(env),
-                # anonymous="allow",
-                config=config, 
-                # mode="disabled"
-                )
+        wandb.init(
+            project="Imitation",
+            name=model_name + "_{0}".format(env),
+            group=model_name,
+            # anonymous="allow",
+            config=config,
+            # mode="disabled"
+        )
         for eval in eval_type:
-            concat_results=pd.concat(results[eval])
-            mean=concat_results.mean()
-            std=concat_results.std()
-            confidence_interval=2*std / np.sqrt(len(seeds))
-            data={}
+            concat_results = pd.concat(results[eval])
+            mean = concat_results.mean()
+            std = concat_results.std()
+            confidence_interval = 2 * std / np.sqrt(len(seeds))
+            data = {}
             for key in mean.keys():
-                data[key]=[mean[key],confidence_interval[key]]
-            final_results=pd.DataFrame.from_dict(data, orient='index', columns=['mean','confidence_interval'])
-            table=wandb.Table(dataframe=final_results)
-            wandb.log({"Statistics over seeds {0}".format(eval) : table})
+                data[key] = [mean[key], confidence_interval[key]]
+            final_results = pd.DataFrame.from_dict(
+                data, orient="index", columns=["mean", "confidence_interval"]
+            )
+            table = wandb.Table(dataframe=final_results)
+            wandb.log({"Statistics over seeds {0}".format(eval): table})
         wandb.run.finish()
 
-# reset the config to config init
-config=config_init.copy()
 
-run_experiment(model_name='Imitation_CNN', params={})
+# reset the config to config init
+config = config_init.copy()
+
+run_experiment(model_name="Imitation_CNN", params={})
--- imitation/train_mint.py
+++ imitation/train_mint.py
@@ -45,59 +45,69 @@ config_init={
 seeds=[1,2,3,4,5]
 
 def set_seed(seed):
-  torch.manual_seed(seed)
-  torch.cuda.manual_seed(seed)
-  np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    np.random.seed(seed)
 
-envs=['MoveToRegion', 'MoveToCorner', 'MakeLine']
-epochs=[1000, 2500, 5000]
-eval_type=['Demo','TestJitter']
+
+envs = ["MoveToRegion", "MoveToCorner", "MakeLine"]
+epochs = [1000, 2500, 5000]
+eval_type = ["Demo", "TestJitter"]
 
 def run_experiment(model_name, params):
-  print('Running experiment {0}'.format(model_name))
-  for key in params:
-    config[key]=params[key]
-  for i,env in enumerate(envs):
-    results={}
-    for eval in eval_type:
-      results[eval]=[]
-    config['epochs']=epochs[i]
-    for seed in seeds:
-      set_seed(seed)
-      model_name_seed=model_name+"_{0}_seed={1}".format(env,seed)
-      config['model_name']=model_name_seed
-      wandb.init(project="Seeds_imitation", name=model_name_seed,
-              # anonymous="allow",
-              config=config, 
-              # mode="disabled"
-              )
-      agent=ImitationAgent(config, env_name=env)
-      agent.train()
-      for eval in eval_type:
-        agent.set_env(eval)
-        agent.evaluate_policy()
-        result=agent.report_results(eval)
-        results[eval].append(result)
-      wandb.run.finish()
-    wandb.init(project="Seeds_imitation", name=model_name+"_{0}".format(env),
-                  # anonymous="allow",
-                  config=config, 
-                  # mode="disabled"
-                  )
-    for eval in eval_type:
-      concat_results=pd.concat(results[eval])
-      mean=concat_results.mean()
-      std=concat_results.std()
-      confidence_interval=2*std / np.sqrt(len(seeds))
-      data={}
-      for key in mean.keys():
-          data[key]=[mean[key],confidence_interval[key]]
-      final_results=pd.DataFrame.from_dict(data, orient='index', columns=['mean','confidence_interval'])
-      table=wandb.Table(dataframe=final_results)
-      wandb.log({"Statistics over seeds {0}".format(eval) : table})
-    wandb.run.finish()
+    print("Running experiment {0}".format(model_name))
+    for key in params:
+        config[key] = params[key]
+    for i, env in enumerate(envs):
+        results = {}
+        for eval in eval_type:
+            results[eval] = []
+        config["epochs"] = epochs[i]
+        for seed in seeds:
+            set_seed(seed)
+            model_name_seed = model_name + "_{0}_seed={1}".format(env, seed)
+            config["model_name"] = model_name_seed
+            wandb.init(
+                project="Seeds_imitation",
+                name=model_name_seed,
+                group=model_name,
+                # anonymous="allow",
+                config=config,
+                # mode="disabled"
+            )
+            agent = ImitationAgent(config, method="MINT", env_name=env)
+            agent.train()
+            for eval in eval_type:
+                agent.set_env(eval)
+                agent.evaluate_policy()
+                result = agent.report_results(eval)
+                results[eval].append(result)
+            wandb.run.finish()
+        wandb.init(
+            project="Seeds_imitation",
+            name=model_name + "_{0}".format(env),
+            group=model_name,
+            # anonymous="allow",
+            config=config,
+            # mode="disabled"
+        )
+        for eval in eval_type:
+            concat_results = pd.concat(results[eval])
+            mean = concat_results.mean()
+            std = concat_results.std()
+            confidence_interval = 2 * std / np.sqrt(len(seeds))
+            data = {}
+            for key in mean.keys():
+                data[key] = [mean[key], confidence_interval[key]]
+            final_results = pd.DataFrame.from_dict(
+                data, orient="index", columns=["mean", "confidence_interval"]
+            )
+            table = wandb.Table(dataframe=final_results)
+            wandb.log({"Statistics over seeds {0}".format(eval): table})
+        wandb.run.finish()
+
 
 # reset the config to config init
-config=config_init.copy()
+config = config_init.copy()
 
-run_experiment(model_name='Imitation_MINT', params={})
+run_experiment(model_name="Imitation_MINT_memory", params={})
