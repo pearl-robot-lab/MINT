278c9c91734ee76ae93942f9b3859b22db5e3960
Alonso94 <ay20-5-1994@hotmail.com>
2023-01-19 17:55:16 +0100

    gitignore

--- download_datasets.sh
+++ download_datasets.sh
@@ -6,8 +6,8 @@ cd datasets
 # define and array with the download links
 links=(
 "https://zenodo.org/record/7105244/files/CLEVRER_20videos_128frames_480x320_20training_80validation.pt"
-# "https://zenodo.org/record/7062781/files/Simitate_80videos_100frames_480x272_4tasks_20training_5validation.pt"
-# "https://zenodo.org/record/7062781/files/MIME_80videos_50frames_320x240_4tasks_20training_5validation.pt"
+"https://zenodo.org/record/7062781/files/Simitate_80videos_100frames_480x272_4tasks_20training_5validation.pt"
+"https://zenodo.org/record/7062781/files/MIME_80videos_50frames_320x240_4tasks_20training_5validation.pt"
 # "https://zenodo.org/record/6568081/files/MIME_dataset_320x240_2tasks_3training_1validation.pt" 
 # "https://zenodo.org/record/6568081/files/Simitate_dataset_480x272_2tasks_3training_1validation.pt"
 )
